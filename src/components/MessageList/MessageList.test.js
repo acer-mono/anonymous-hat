bc4f24d416fedbc4879bc29b746bec407ccf88cd
acer-mono <margaritastar92@gmail.com>
2020-10-16 22:50:07 +0700

    add tests for MessageList

--- src/components/MessageList/MessageList.test.js
+++ src/components/MessageList/MessageList.test.js
@@ -1,6 +1,8 @@
 import {shallow} from "enzyme/build";
 import MessagesList from "./MessagesList";
 import React from "react";
+import renderer from "react-test-renderer";
+import Message from "../Message";
 
 describe('MessagesList', () => {
     test('add 3 elements', () => {
@@ -17,6 +19,26 @@ describe('MessagesList', () => {
         const messages = [];
         const component = shallow(<MessagesList messages={messages}/>);
         expect(component.find('Message')).toHaveLength(0);
+    });
 
+    test('MessageList test with snapshot (empty message list)', () => {
+        const messages = [];
+        const component = renderer.create(
+            <MessagesList messages={messages}/>,
+        );
+        let componentSnapshot = component.toJSON();
+        expect(componentSnapshot).toMatchSnapshot();
+    });
+
+    test('MessageList test with snapshot (message list with 3 messages)', () => {
+        const messages = [
+            {user: "first", message: "firstMessage"},
+            {user: "second", message: "secondMessage"},
+            {user: "second", message: "secondMessage"}];
+        const component = renderer.create(
+            <MessagesList messages={messages}/>,
+        );
+        let componentSnapshot = component.toJSON();
+        expect(componentSnapshot).toMatchSnapshot();
     });
 });
