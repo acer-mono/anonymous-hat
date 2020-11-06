e223333f007fae30d209401d12fcae8ea6b92888
acer-mono <margaritastar92@gmail.com>
2020-11-06 21:04:16 +0700

    add Formik

--- src/views/RegistrationView/RegistrationView.js
+++ src/views/RegistrationView/RegistrationView.js
@@ -1,95 +1,97 @@
 import React from 'react';
 import styles from './styles.modules.css';
+import { Formik } from 'formik';
 import apiServices from '../../apiServices';
 
 class RegistrationView extends React.Component {
   constructor() {
     super();
     this.state = {
-      user: '',
-      password: '',
       passwordConfirm: '',
       error: null,
-      success: null,
+      result: null,
     };
   }
 
-  handleSend(e) {
-    this.setState({
-      error: null,
-      success: null,
-    });
-
-    const { user, password } = this.state;
+  handleSend(values) {
     apiServices.user
-      .create({
-        nickname: user,
-        password: password,
-      })
-      .then(() => this.setState({ success: 'Пользователь успешно зарегистрирован' }))
+      .create(values)
+      .then(() => this.setState({ result: 'Пользователь успешно зарегистрирован' }))
       .then(() => setTimeout(() => this.props.history.push('/login'), 2000))
       .catch(error => this.setState({ error: `ERROR: ${error.response.data.error}` }));
-    e.preventDefault();
   }
 
   render() {
-    const { user, password, passwordConfirm, error, success } = this.state;
+    const { passwordConfirm, error, result } = this.state;
 
-    let alert;
-    if (error) {
-      alert = (
-        <div className="alert alert-danger" role="alert">
-          {error}
-        </div>
-      );
-    }
-    if (success) {
-      alert = (
-        <div className="alert alert-success" role="alert">
-          {success}
-        </div>
-      );
-    }
     return (
       <div className={styles.main}>
-        <form className={styles.form} onSubmit={e => this.handleSend(e)}>
-          <h4 className="text-center">Регистрация</h4>
-          {alert}
-          <input
-            id="nick"
-            name="nick"
-            value={user}
-            className="form-control mb-4"
-            type="text"
-            placeholder="Your name..."
-            onChange={e => this.setState({ user: e.target.value })}
-          />
-          <input
-            className="form-control mb-4"
-            type="password"
-            value={password}
-            id="password"
-            name="password"
-            placeholder="Your password..."
-            onChange={e => this.setState({ password: e.target.value })}
-          />
-          <input
-            className="form-control mb-4"
-            type="password"
-            value={passwordConfirm}
-            id="password"
-            name="password"
-            placeholder="Confirm your password..."
-            onChange={e => this.setState({ passwordConfirm: e.target.value })}
-          />
-          <input
-            type="submit"
-            className="btn btn-primary btn-block"
-            id="send"
-            value="Зарегистрироваться"
-            onClick={e => this.handleSend(e)}
-          />
-        </form>
+        <Formik
+          initialValues={{ nickname: '', password: '' }}
+          validate={values => {
+            const errors = {};
+            if (!values.nickname) {
+              errors.nickname = 'Введите никнейм';
+            }
+            if (!values.password) {
+              errors.password = 'Введите пароль';
+            }
+            if (values.password.length < 7) {
+              errors.password = 'Длина пароля должна быть больше 6 символов';
+            }
+            return errors;
+          }}
+          onSubmit={values => {
+            this.handleSend(values);
+          }}>
+          {({ values, errors, touched, handleChange, handleBlur, handleSubmit }) => (
+            <form className={styles.form} onSubmit={handleSubmit}>
+              <h4 className="text-center">Регистрация</h4>
+              {error ? <div className="alert alert-danger">{error}</div> : null}
+              {result ? <div className="alert alert-success">{result}</div> : null}
+              {errors.nickname && touched.nickname && (
+                <div style={{ color: 'red' }}>{errors.nickname}</div>
+              )}
+              <input
+                id="nick"
+                className="form-control mb-4"
+                type="text"
+                name="nickname"
+                placeholder="Your name..."
+                value={values.nickname}
+                onChange={handleChange}
+                onBlur={handleBlur}
+              />
+              {errors.password && touched.password && (
+                <div style={{ color: 'red' }}>{errors.password}</div>
+              )}
+              <input
+                className="form-control mb-4"
+                type="password"
+                name="password"
+                placeholder="Your password..."
+                value={values.password}
+                onChange={handleChange}
+                onBlur={handleBlur}
+              />
+              <input
+                className="form-control mb-4"
+                type="password"
+                value={passwordConfirm}
+                id="password"
+                name="password"
+                placeholder="Confirm your password..."
+                onChange={e => this.setState({ passwordConfirm: e.target.value })}
+              />
+              <input
+                type="submit"
+                className="btn btn-primary btn-block"
+                id="send"
+                value="Зарегистрироваться"
+              />
+            </form>
+          )}
+        </Formik>
       </div>
     );
   }
