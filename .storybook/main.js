577263b303c28ba495f8e93e59141ab7e979e9d4
acer-mono <margaritastar92@gmail.com>
2020-12-02 16:18:16 +0700

    fix storybook config

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,6 @@
 const webpackConfig = require('../configs/webpack.common.js');
+const path = require('path');
+
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
@@ -6,14 +8,21 @@ module.exports = {
     '@storybook/addon-links',
     '@storybook/addon-essentials'
   ],
-  webpackFinal: config => ({
-    ...config,
-    resolve: {
-      ...config.resolve,
-      alias: {
+  webpackFinal: config => {
+    config.resolve.alias = {
         ...config.resolve.alias,
         ...webpackConfig.resolve.alias
-      }
-    }
-  })
+      };
+
+    config.module.rules = config.module.rules.filter(
+        f => f.test.toString() !== '/\\.css$/'
+    );
+
+    config.module.rules.push({
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader'],
+      include: path.resolve(__dirname, '../'),
+    });
+    return config;
+  }
 }
