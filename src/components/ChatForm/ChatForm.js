3ff69ff389d02a9315ad16afda30685c672bbb16
acer-mono <margaritastar92@gmail.com>
2020-12-22 15:27:08 +0700

    функциональный компонент со state

--- src/components/ChatForm/ChatForm.js
+++ src/components/ChatForm/ChatForm.js
@@ -1,76 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
-class ChatForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      title: '',
-      isPrivate: false,
-      error: '',
-    };
-  }
+function ChatForm(props) {
+  const [title, setTitle] = useState('');
+  const [error, setError] = useState('');
+  const [isPrivate, setIsPrivate] = useState(false);
 
-  validate() {
-    this.setState({
-      error: '',
-    });
-    if (this.state.title.length === 0) {
-      this.setState({
-        error: 'Введите название чата',
-      });
+  function validate() {
+    setError('');
+    if (title.length === 0) {
+      setError('Введите название чата');
       return false;
     }
     return true;
   }
 
-  handleSubmit(event) {
+  function handleSubmit(event) {
     event.preventDefault();
 
-    if (this.validate()) {
-      this.props.handleSubmit({ title: this.state.title, isPrivate: this.state.isPrivate });
-      this.setState({ title: '', isPrivate: false });
+    if (validate()) {
+      props.handleSubmit({ title: title, isPrivate: isPrivate });
+      setTitle('');
+      setIsPrivate(false);
     }
   }
 
-  render() {
-    const { title, isPrivate, error } = this.state;
-
-    return (
-      <>
-        <div>{error && <span style={{ color: 'red' }}>{error}</span>}</div>
-        <form onSubmit={e => this.handleSubmit(e)}>
-          <div className="form-check">
-            <input
-              className="form-check-input"
-              type="checkbox"
-              checked={isPrivate}
-              onChange={event => this.setState({ isPrivate: event.target.checked })}
-              id="isPrivate"
-            />
-            <label className="form-check-label" htmlFor="isPrivate">
-              Секретный
-            </label>
+  return (
+    <>
+      <div>{error && <span style={{ color: 'red' }}>{error}</span>}</div>
+      <form onSubmit={e => handleSubmit(e)}>
+        <div className="form-check">
+          <input
+            className="form-check-input"
+            type="checkbox"
+            checked={isPrivate}
+            onChange={event => setIsPrivate(event.target.checked)}
+            id="isPrivate"
+          />
+          <label className="form-check-label" htmlFor="isPrivate">
+            Секретный
+          </label>
+        </div>
+        <div className="input-group md-form form-sm form-1 pl-0">
+          <div className="input-group-prepend">
+            <button className="btn btn-outline-success" type="submit">
+              Create
+            </button>
           </div>
-          <div className="input-group md-form form-sm form-1 pl-0">
-            <div className="input-group-prepend">
-              <button className="btn btn-outline-success" type="submit">
-                Create
-              </button>
-            </div>
-            <input
-              className="form-control"
-              type="text"
-              placeholder="Enter chat name..."
-              aria-label="Create"
-              value={title}
-              onChange={event => this.setState({ title: event.target.value })}
-            />
-          </div>
-        </form>
-      </>
-    );
-  }
+          <input
+            className="form-control"
+            type="text"
+            placeholder="Enter chat name..."
+            aria-label="Create"
+            value={title}
+            onChange={event => setTitle(event.target.value)}
+          />
+        </div>
+      </form>
+    </>
+  );
 }
 
 ChatForm.propTypes = {
